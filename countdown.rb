fc2512ad02edb4c93f3136ce55838f9c30649ea9
Ana Bebic <anabebic.x@gmail.com>
2018-06-25 11:28:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,10 @@
-def countdown(x)
-while x > 0
-  puts "#{x} SECOND(S)!"
-  sleep 1
-  x -= 1
- end	 
-+return "HAPPY NEW YEAR!"
-+end
+def countdown(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
